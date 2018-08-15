1f8150b4a59a6857eaabfa81c40bc8f41d6c1723
Ujjwal <ujjrisal@gmail.com>
2018-08-15 10:04:02 +1000

    Initial commit

---
+++ KeyIs/src/main/java/com/keyis/KeyIsApplication.java
@@ -0,0 +1,12 @@
+package com.keyis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyIsApplication.class, args);
+	}
+}
